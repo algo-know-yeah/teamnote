b1086095a22a2e47cb10962630e031c15a5f7d80
Neogulee <65273504+Neogulee@users.noreply.github.com>
2020-07-20 18:07:34 +0900

    fix: bug fix

--- src/string/trie.cpp
+++ src/string/trie.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 const int ALPHABETS = 26;
 
-int chToIdx(char ch) { return ch - 'A'; }
+int chToIdx(char ch) { return ch - 'a'; }
 struct Trie {
 	bool check = false;
 	Trie* chil[ALPHABETS];
@@ -19,31 +19,47 @@ struct Trie {
 				delete chil[i];
 	}
 	void insert(string& s, int idx = 0) {
-		if (idx == s.size() - 1) {
+		if (idx == s.size()) {
 			check = true;
 			return;
 		}
 		int next = chToIdx(s[idx]);
 		if (chil[next] == NULL)
-			chil[next] = new Trie;
+			chil[next] = new Trie();
 		chil[next]->insert(s, idx + 1);
 	}
 	bool find(string& s, int idx = 0) {
-		if (idx == s.size() - 1)
+		if (idx == s.size())
 			return check;
 		int next = chToIdx(s[idx]);
 		if (chil[next] == NULL)
 			return false;
-		chil[next]->find(s, idx + 1);
+		return chil[next]->find(s, idx + 1);
 	}
 };
 
 int main(void)
 {
-	Trie trie;
-	trie.insert(string("ASF"));
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-	cout << trie.find(string("DA")) << endl;
-	cout << trie.find(string("AS")) << endl;
-	cout << trie.find(string("ASF")) << endl;
+	int N, M;
+	cin >> N >> M;
+	Trie trie;
+	for (int i = 0; i < N; i++)
+	{
+		string s;
+		cin >> s;
+		trie.insert(s);
+	}
+	int ret = 0;
+	for (int i = 0; i < M; i++)
+	{
+		string s;
+		cin >> s;
+		if (trie.find(s))
+			ret++;
+	}
+	cout << ret << endl;
 }
